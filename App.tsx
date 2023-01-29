22ec78eefa7276496091656bc5d30c5dcad63067
yoontaeho <pln0302@yonsei.ac.kr>
2023-01-29 20:33:09 +0900

    Initialized app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { NativeBaseProvider } from "native-base";
+
+import Init from "./src/Init";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <NativeBaseProvider>
+          <Init />
+        </NativeBaseProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Init.tsx
@@ -0,0 +1,8 @@
+import { Box } from "native-base";
+import { FC } from "react";
+
+const Init: FC = () => {
+  return <Box>Hello React Native!</Box>;
+};
+
+export default Init;
